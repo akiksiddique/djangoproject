9a28540f8d4385246f5464447fd3a2307a3e51f3
mr.akik <mr.akik@yahoo.com>
2017-09-29 14:43:33 +0600

    edited

---
+++ project/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from app.views import *
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'project.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', home),
+    url(r'^admin/', include(admin.site.urls)),
+]
